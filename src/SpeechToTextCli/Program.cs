3d28479a7c4ccb4bca0e7765c70a30bcb377a753
Grzegorz Gwóźdź <g.gwozdz@hotmail.com>
2025-01-22 17:22:01 +0100

    refactor: simplify Program.cs by extracting host creation logic
    - Refactored Program.cs to reduce complexity by moving host creation to HostBuilderFactory.
    - This change centralizes dependency injection and logging configuration, improving code organization.

--- src/SpeechToTextCli/Program.cs
+++ src/SpeechToTextCli/Program.cs
@@ -1,33 +1,7 @@
 ﻿using System.CommandLine;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using SpeechToTextCli.Extensions;
-using SpeechToTextProcessor.Core;
+using SpeechToTextCli.Configuration;
 
-var host = CreateHostBuilder(args).Build();
-
+var host = HostBuilderFactory.CreateHostBuilder(args).Build();
 var rootCommand = host.Services.GetRequiredService<RootCommand>();
 await rootCommand.InvokeAsync(args);
-return;
-
-static IHostBuilder CreateHostBuilder(string[] args)
-{
-    return Host.CreateDefaultBuilder(args)
-        .ConfigureAppConfiguration((context, config) =>
-        {
-            config.AddJsonFile("appsettings.json", false, true);
-        })
-        .ConfigureLogging((context, logging) =>
-        {
-            logging.AddConfiguration(context.Configuration.GetSection("Logging"));
-            logging.AddConsoleLogger(context.Configuration);
-        })
-        .ConfigureServices((context, services) =>
-        {
-            services.AddLogging();
-            services.AddSpeechToTextAdapter();
-            services.AddCommands();
-        });
-}
